bb8557d7fc0cef60875f17fa19336a1ac1ee83dd
Abdelghafour AOUAD <abdelghafour.aouad0@gmail.com>
2023-01-16 09:54:02 +0100

    init tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        bluee: '#2d6bad',
+        gold: '#FFD700',
+        goldenrod: '#DAA520',
+        goldenyellow: '#FFDF00',
+        goldtransparent: '#ffd90033',
+        transparentBlack: '#00000066',
+      }
+    },
+  },
+
+  plugins: [],
+}
