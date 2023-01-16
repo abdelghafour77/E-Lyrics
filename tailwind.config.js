c5e03e9833d4e5ce8304ff743caedac3a806ac67
Abdelghafour AOUAD <abdelghafour.aouad0@gmail.com>
2023-01-16 16:17:24 +0100

    update colors in tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        bluee: '#2d6bad',
-        gold: '#FFD700',
-        goldenrod: '#DAA520',
-        goldenyellow: '#FFDF00',
-        goldtransparent: '#ffd90033',
-        transparentBlack: '#00000066',
+
+        black: '#222831',
+        dark: '#393E46',
+        teal: '#00ADB5',
+        grey: '#EEEEEE',
+        white: '#FAFAFA',
+        // purple: '#4D148C',
+        // orange: '#FF6600',
+        // orange_h: '#d55500',
+        // white: '#FAFAFA',
+        // bluee: '#2d6bad',
+        // gold: '#FFD700',
+        // goldenrod: '#DAA520',
+        // goldenyellow: '#FFDF00',
+        // goldtransparent: '#ffd90033',
+        // transparentBlack: '#00000066',
       }
     },
   },
