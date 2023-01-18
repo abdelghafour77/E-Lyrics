732969bcc6d79578560d5f55449c11ee50f5cb20
Abdelghafour AOUAD <abdelghafour.aouad0@gmail.com>
2023-01-18 17:11:36 +0100

    update database

--- database/e-lyrics.sql
+++ database/e-lyrics.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : lun. 16 jan. 2023 à 22:01
--- Version du serveur : 8.0.27
--- Version de PHP : 7.4.26
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 18, 2023 at 04:11 PM
+-- Server version: 8.0.27
+-- PHP Version: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `e-lyrics`
+-- Database: `e-lyrics`
 --
 CREATE DATABASE IF NOT EXISTS `e-lyrics` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 USE `e-lyrics`;
@@ -26,15 +26,15 @@ USE `e-lyrics`;
 -- --------------------------------------------------------
 
 --
--- Structure de la table `albums`
+-- Table structure for table `albums`
 --
 
 DROP TABLE IF EXISTS `albums`;
 CREATE TABLE IF NOT EXISTS `albums` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) COLLATE utf8_bin NOT NULL,
-  `description` text COLLATE utf8_bin,
-  `image` text COLLATE utf8_bin NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_bin,
+  `image` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `artist_id` int NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
@@ -45,15 +45,15 @@ CREATE TABLE IF NOT EXISTS `albums` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `artists`
+-- Table structure for table `artists`
 --
 
 DROP TABLE IF EXISTS `artists`;
 CREATE TABLE IF NOT EXISTS `artists` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(120) COLLATE utf8_bin NOT NULL,
-  `picture` text COLLATE utf8_bin NOT NULL,
-  `biography` text COLLATE utf8_bin NOT NULL,
+  `name` varchar(120) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `picture` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `biography` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `created_at` timestamp NOT NULL,
   `updated_at` timestamp NOT NULL,
   `created_by` int NOT NULL,
@@ -64,31 +64,31 @@ CREATE TABLE IF NOT EXISTS `artists` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `categories`
+-- Table structure for table `categories`
 --
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) COLLATE utf8_bin NOT NULL,
-  `description` text COLLATE utf8_bin NOT NULL,
-  `image` text COLLATE utf8_bin NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `image` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `songs`
+-- Table structure for table `songs`
 --
 
 DROP TABLE IF EXISTS `songs`;
 CREATE TABLE IF NOT EXISTS `songs` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) COLLATE utf8_bin NOT NULL,
-  `description` text COLLATE utf8_bin,
-  `lyrics` text COLLATE utf8_bin NOT NULL,
-  `image` text COLLATE utf8_bin,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_bin,
+  `lyrics` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `image` text CHARACTER SET utf8 COLLATE utf8_bin,
   `type` int NOT NULL,
   `album_id` int DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -100,20 +100,31 @@ CREATE TABLE IF NOT EXISTS `songs` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `full_name` varchar(60) COLLATE utf8_bin NOT NULL,
-  `email` varchar(60) COLLATE utf8_bin NOT NULL,
-  `admin` int NOT NULL,
-  `created_at` timestamp NOT NULL,
-  `updated_at` timestamp NOT NULL,
-  `deleted_at` timestamp NOT NULL,
+  `full_name` varchar(60) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `email` varchar(60) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `password` text COLLATE utf8_bin NOT NULL,
+  `admin` int NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `full_name`, `email`, `password`, `admin`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(2, 'Nick', 'Doe', '$2y$10$j8OJApvra4z9L41QZNi0g.fOrxgY16HcSJ8IrsL.8j1wXC/xvSzKK', 0, '2023-01-18 10:48:15', '2023-01-18 10:48:15', NULL),
+(3, 'Meredith Spencer', 'vumedet@mailinator.com', '$2y$10$YK4uHJ7jU2/TwbUq0cKU2OVvkRDtpidkmGRaLrLHyGyK8.tCBXq6y', 0, '2023-01-18 10:49:15', '2023-01-18 10:49:15', NULL),
+(4, 'Rama Castaneda', 'syla@mailinator.com', '$2y$10$yGo1SMv3gsGjo9rPvyj0/ur3isocgHQRa2f7FYPJDdHd1cONXxDhe', 0, '2023-01-18 10:56:15', '2023-01-18 10:56:15', NULL),
+(5, 'Hiroko Bernard', 'xegexije@mailinator.com', '$2y$10$mAdz.8mw9AAtEGho6R1qsuxg0EVcGur.rfiwzi8jFFo2rZInxCDty', 0, '2023-01-18 10:57:19', '2023-01-18 10:57:19', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
